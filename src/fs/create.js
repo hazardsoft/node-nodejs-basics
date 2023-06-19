0beabb70e6a053b1b8588d90913b9fcef71177d3
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-19 13:19:53 +0300

    init: start nodejs-basics task
    implement file creation in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+
 const create = async () => {
-    // Write your code here 
+    const pathToFile = fileURLToPath(
+        new URL("./files/fresh.txt", import.meta.url)
+    );
+    let isFileExist = false;
+    try {
+        await fs.access(pathToFile, fs.constants.F_OK);
+        isFileExist = true;
+    } catch (e) {
+        console.error("error occurred while calling fs.access:", e);
+    }
+    if (isFileExist) {
+        throw new Error("FS operation failed");
+    }
+    await fs.writeFile(pathToFile, "I am fresh and young");
 };
 
 await create();
