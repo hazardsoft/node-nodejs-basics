c3b16bb04b762ef1e6fb66d8833123d9c42991a5
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-19 14:54:50 +0300

    feat: implement env vars read with "RSS_" prefix

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVars = [];
+    const prefix = "RSS_";
+    for (const env in process.env) {
+        if (env.startsWith(prefix)) {
+            envVars.push([env, process.env[env]]);
+        }
+    }
+    console.log(envVars.map((pair) => `${pair[0]}=${pair[1]}`).join("; "));
 };
 
 parseEnv();
