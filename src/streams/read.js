e2b2c086a85d018f9b5179409478854756490a77
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-19 15:46:59 +0300

    feat: implement file read with readable stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = fileURLToPath(
+        new URL("files/fileToRead.txt", import.meta.url)
+    );
+    const readableStream = fs.createReadStream(pathToFile, {
+        encoding: "utf8",
+    });
+    readableStream.pipe(process.stdout);
 };
 
 await read();
