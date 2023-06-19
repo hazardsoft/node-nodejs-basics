bc213b30ff75de91fb042b7201a92cdb53f0c5bd
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-19 16:11:04 +0300

    feat: implement file archiving with gzip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { pipeline } from "node:stream/promises";
+import { fileURLToPath } from "node:url";
+import fs from "node:fs";
+import { createGzip } from "node:zlib";
+
 const compress = async () => {
-    // Write your code here 
+    const srcFilePath = fileURLToPath(
+        new URL("files/fileToCompress.txt", import.meta.url)
+    );
+    const destFilePath = fileURLToPath(
+        new URL("files/archive.gz", import.meta.url)
+    );
+    const readableStream = fs.createReadStream(srcFilePath);
+    const writeableStream = fs.createWriteStream(destFilePath);
+    const gzip = createGzip();
+    await pipeline(readableStream, gzip, writeableStream);
 };
 
 await compress();
