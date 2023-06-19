496cfb39ac3dd0d7227c0b8e2b85d2ff218b16ef
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-19 15:35:11 +0300

    feat: implement hash calculation with SHA256 in hex

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { createHash } from "node:crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const pathToFile = fileURLToPath(
+        new URL("files/fileToCalculateHashFor.txt", import.meta.url)
+    );
+    const fileBuffer = await fs.readFile(pathToFile);
+    const hexHash = createHash("sha256").update(fileBuffer).digest("hex");
+    console.log(hexHash);
 };
 
 await calculateHash();
