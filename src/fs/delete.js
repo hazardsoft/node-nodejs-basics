00078da3d8dbd2852a2f0dd011a70632d76c8391
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-19 13:57:47 +0300

    feat: implement file remove

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import checkIfFileExists from "./utils.js";
+
 const remove = async () => {
-    // Write your code here 
+    const pathToFile = fileURLToPath(
+        new URL("files/fileToRemove.txt", import.meta.url)
+    );
+    const isFileExist = await checkIfFileExists(pathToFile);
+    if (!isFileExist) {
+        throw new Error("FS operation failed");
+    }
+    await fs.rm(pathToFile, { force: true });
 };
 
 await remove();
