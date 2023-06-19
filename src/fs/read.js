515fd7e01997f35632276cd4b751281401c643ab
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-19 14:33:14 +0300

    feat: implement file read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import checkIfFileExists from "./utils.js";
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = fileURLToPath(
+        new URL("files/fileToRead.txt", import.meta.url)
+    );
+    const isFileExist = await checkIfFileExists(pathToFile);
+    if (!isFileExist) {
+        throw new Error("FS operation failed");
+    }
+    const fileContent = await fs.readFile(pathToFile, { encoding: "utf8" });
+    console.log(fileContent);
 };
 
 await read();
