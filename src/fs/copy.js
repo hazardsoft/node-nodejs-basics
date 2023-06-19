22efc482e3e6a917e62bbbac17c4603b2743d91b
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-19 13:44:12 +0300

    feat: implement folder copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import checkIfFileExists from "./utils.js";
+
 const copy = async () => {
-    // Write your code here 
+    const srcPath = fileURLToPath(new URL("files", import.meta.url));
+    const destPath = fileURLToPath(new URL("files_copy", import.meta.url));
+
+    const isSrcFolderExist = await checkIfFileExists(srcPath);
+    const isDestFolderExist = await checkIfFileExists(destPath);
+    if (!isSrcFolderExist || isDestFolderExist) {
+        throw new Error("FS operation failed");
+    }
+    await fs.cp(srcPath, destPath, { recursive: true });
 };
 
 await copy();
