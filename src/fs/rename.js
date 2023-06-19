7a165a0d64e0dbe08719c1c437097c5a5d24f6cc
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-19 13:50:20 +0300

    feat: implement file rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import checkIfFileExists from "./utils.js";
+
 const rename = async () => {
-    // Write your code here 
+    const oldPath = fileURLToPath(
+        new URL("files/wrongFilename.txt", import.meta.url)
+    );
+    const newPath = fileURLToPath(
+        new URL("files/properFilename.md", import.meta.url)
+    );
+
+    const isOldFileExist = await checkIfFileExists(oldPath);
+    const isNewFileExist = await checkIfFileExists(newPath);
+    if (!isOldFileExist || isNewFileExist) {
+        throw new Error("FS operation failed");
+    }
+    await fs.rename(oldPath, newPath);
 };
 
 await rename();
