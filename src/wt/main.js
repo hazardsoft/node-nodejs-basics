a9ede45e1fbf74ded6a468652aee76d91d4daaa5
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-19 17:04:31 +0300

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker } from "node:worker_threads";
+import os from "node:os";
+import { URL } from "node:url";
+
+const successfulStatus = "fulfilled";
+
 const performCalculations = async () => {
-    // Write your code here
+    let startNumber = 10;
+    const numofCPUs = os.cpus().length;
+    const promises = [];
+    for (let i = 0; i < numofCPUs; i++) {
+        promises.push(runWorker(startNumber));
+        startNumber++;
+    }
+    const results = await Promise.allSettled(promises);
+    const transformedResults = results.map((result) => {
+        return {
+            status: result.status === successfulStatus ? "resolved" : "error",
+            data: result.status === successfulStatus ? result.value : null,
+        };
+    });
+    console.log(transformedResults);
 };
+
+async function runWorker(data) {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(new URL("./worker.js", import.meta.url), {
+            workerData: data,
+        });
+        worker.on("message", resolve);
+        worker.on("error", reject);
+        worker.on("exit", reject);
+    });
+}
 
 await performCalculations();
