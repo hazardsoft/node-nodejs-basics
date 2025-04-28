839c4dfd44ed526c6b42eee9a63316ba45ad2627
Henadzi Shutko <hazardsoft@gmail.com>
2025-04-28 19:41:36 +0300

    feat: implement worker threads

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
+import {workerData, parentPort} from "worker_threads"
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    if (typeof workerData === "number") {
+        const result = nthFibonacci(workerData)
+        parentPort.postMessage(result);
+    } else {
+        throw new Error("invalid data passed to worker", workerData);
+    }
 };
 
 sendResult();
