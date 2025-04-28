907f11c5c6b401c1cafd3041a45308d0d6fab327
Henadzi Shutko <hazardsoft@gmail.com>
2025-04-28 14:47:15 +0300

    feat: add implementation of cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    const outputPairs = []
+    const args = process.argv.slice(2);
+    for (let i = 0; i < args.length; i += 2) {
+        outputPairs.push(`${args[i]} is ${args[i+1]}`)
+    }
+    console.log(outputPairs.join(", "));
 };
 
 parseArgs();
