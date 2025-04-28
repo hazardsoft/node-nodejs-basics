45940681c1da7a638615c9d96971dbad9567af46
Henadzi Shutko <hazardsoft@gmail.com>
2025-04-28 18:49:03 +0300

    feat: add compress/decompress functions with zlib stream api

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import path from "path"
+import {fileURLToPath} from "url"
+import {createReadStream, createWriteStream} from "fs"
+import {createUnzip} from "zlib"
+import { pipeline } from "stream";
+
 const decompress = async () => {
-    // Write your code here 
+    const inputFileName = "archive.gz";
+    const inputFilePath = path.resolve(fileURLToPath(import.meta.url), "..", "files", inputFileName);
+
+    const outputFileName = "fileToCompress.txt";
+    const outputFilePath = path.resolve(fileURLToPath(import.meta.url), "..", "files", outputFileName);
+
+    const inputStream = createReadStream(inputFilePath);
+    const outputStream = createWriteStream(outputFilePath);
+
+    pipeline(
+        inputStream,
+        createUnzip(),
+        outputStream,
+        (err) => {
+            if (err) console.error("pipeline failed", err);
+        }
+    )
 };
 
 await decompress();
